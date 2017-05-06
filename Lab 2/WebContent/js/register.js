1525dda6b38ad6a9025f7304473ce50041e61330
Adriana <adriana.barrachina01@estudiant.upf.edu>
2017-05-06 13:09:04 +0200

    Primeres comprovacions dels camps del formulari

--- Lab 2/WebContent/js/register.js
+++ Lab 2/WebContent/js/register.js
@@ -10,7 +10,7 @@ var REGISTER = {
 }
 
 function checkInputs(){
-  document.querySelector(".checkables").onchange = function(){
+  /*document.getElementsByClassName("checkables").onkeyup = function(){
     switch (this.id) {
       case "username":
           checkUsername();
@@ -22,28 +22,53 @@ function checkInputs(){
           checkEmail();
         break;
     }
-  }
+  }*/
+
+  document.getElementById("username").addEventListener("keyup", function () {
+    checkUsername();
+  });
+
+  document.getElementById("checkpasswod").addEventListener("blur", function () {
+    checkPassord();
+  });
+
+  document.getElementById("mail").addEventListener("blur", function () {
+    checkEmail();
+  });
 }
 
 function send(){
-	if( name && passwor && mail )
+	if( name && password && mail )
 		document.getElementById("send").disabled = false;
 }
 
 function checkUsername(){
-	name = document.getElementById("username").value.length > 3;
+  var username = document.getElementById("username");
+	name = username.value.length > 3;
+  if(name == "false"){
+    username.style.color = "red";
+  }
+  else {
+    username.style.color = "orange";
+  }
 	//Falta mirar bé com fer la consulta
 	send();
 }
 
 function checkPassord(){
-	var pass = document.getElementById("password").value;
-	passord = pass > 7 && pass == document.getElementById("checkpasswod").value;
+	var pass = document.getElementById("password");
+  var pascheck = document.getElementById("checkpasswod");
+	password = pass.value.length > 7 && pass.value == pascheck.value;
+  pascheck.style.color = pass.style.color = password ? "green" : "red";
 	send();
 }
 
 function checkEmail() {
+    var email = document.getElementById("mail");
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    mai = re.test(document.getElementById("mail").value;);
+    mail = re.test(email.value);
+    email.style.color = mail ? "green" : "red";
     send();
 }
+
+REGISTER.init();
